68acda1959c055c3199b5bdf5a4433b939967f80
Arnold Guevarra Jr <j.guevarra@energin.co>
2022-01-07 10:16:01 +1100

    Updated constant version

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package Nevs Theme Typhography
- * @version 1.0.0
+ * @version 1.0.2
  */
 
 /*
 Plugin Name: Nevs Theme Typhography
 Plugin URI: https://github.com/jmguevarra/nevs-theme-typography
 Description: This is simple plugin that expands theme support customizer api for custom typograhy.
-Version: 1.0.0
+Version: 1.0.2
 Author: Jaime I. Guevarra Jr.
 Author URI: https://github.com/jmguevarra/
 License: GPL3
@@ -22,7 +22,7 @@ if(  !defined('WPINC') ){
 
 //define variables
 define('NEVSTFONT_DIR', dirname(__FILE__));
-define('NEVSTFONT_VERSION', '1.0.0');
+define('NEVSTFONT_VERSION', '1.0.2');
 define('NEVSTFONT_TEXTDOMAIN', 'nevs-theme-typography');
 
 
